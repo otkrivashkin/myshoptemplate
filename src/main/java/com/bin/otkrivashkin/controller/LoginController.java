b787bd997b7cf722b661338b2797d2153ec4cc16
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-07-29 14:31:00 +0300

    impl login method

---
+++ src/main/java/com/bin/otkrivashkin/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.bin.otkrivashkin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String msg,
+                        Model model) {
+
+        if (error != null) {
+            model.addAttribute("error", "Invalid username or password.");
+        }
+
+        if (msg != null) {
+            model.addAttribute("msg", "You have been logged out successfully!");
+        }
+
+        return "login";
+    }
+}
