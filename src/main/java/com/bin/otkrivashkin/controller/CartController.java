22a2848042209a251ab4ad0733febb2e962c29c5
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-07-30 17:27:36 +0300

    impl CartItemController

---
+++ src/main/java/com/bin/otkrivashkin/controller/CartController.java
@@ -0,0 +1,20 @@
+package com.bin.otkrivashkin.controller;
+
+
+import com.bin.otkrivashkin.dao.ProductDao;
+import com.bin.otkrivashkin.model.Cart;
+import com.bin.otkrivashkin.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest/cart")
+public class CartController {
+
+    @Autowired
+    private ProductDao productDao;
+
+}
